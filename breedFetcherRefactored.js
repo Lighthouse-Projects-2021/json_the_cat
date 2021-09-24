d3786ea0db8e4780fd912c3b978fd56693fb091c
Kenia Balestra <kenia.balestra@gmail.com>
2021-09-24 02:05:56 +0000

    Test

---
+++ breedFetcherRefactored.js
@@ -0,0 +1,19 @@
+const request = require('request');
+
+const fetchBreedDescription = function(breedName, callback) {
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+  request(url, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+    }
+    const data = JSON.parse(body);
+    if (data[0]) {
+      callback(null, data[0]["description"]);
+    } else {
+      callback("This dog breed was not found.", null);
+
+    }
+  });
+};
+
+module.exports = { fetchBreedDescription };
